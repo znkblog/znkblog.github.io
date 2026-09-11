1a044ae550ef395b2cb3311913d880dc4546ecb9
zznnkk <zzinnykko@naver.com>
2026-09-11 09:18:44 +0900

    add seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://znkblog.github.io',
+  integrations: [sitemap()],
+});
